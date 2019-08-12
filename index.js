370df3a7a08a67d7c7770a0b774fba0f11190b92
Kamaal Farah <kamaal.f1@gmail.com>
2019-08-12 14:27:16 +0200

    Set up basic server

---
+++ index.js
@@ -0,0 +1,91 @@
+const express = require('express')
+const Sse = require('json-sse')
+const cors = require('cors')
+const { json: bodyParser } = require('body-parser')
+const Sequelize = require('sequelize')
+
+const app = express()
+const stream = new Sse()
+const port = process.env.PORT || 4000
+
+const databaseUrl =
+  process.env.DATABASE_URL ||
+  'postgres://postgres:password@localhost:5432/postgres'
+
+const db = new Sequelize(databaseUrl)
+
+db.sync({ force: false })
+  .then(() => console.log('Database connected'))
+  .catch(console.error)
+
+const User = db.define('user', {
+  name: Sequelize.STRING,
+  password: Sequelize.STRING
+})
+
+const Lobby = db.define('lobby', {
+  game: Sequelize.STRING
+})
+
+User.belongsTo(Lobby)
+Lobby.hasMany(User)
+
+app.use(cors())
+app.use(bodyParser())
+
+app.get('/stream', async (req, res) => {
+  const lobbys = await Lobby.findAll({ include: [User] })
+  const data = JSON.stringify(lobbys)
+
+  stream.updateInit(data)
+  stream.init(req, res)
+})
+
+// Create User
+app.post('/user', async (req, res) => {
+  const { name, password } = req.body
+
+  const entity = await User.create({ name, password })
+  const lobbys = await Lobby.findAll({ include: [User] })
+
+  const data = JSON.stringify(lobbys)
+
+  stream.updateInit(data)
+  stream.send(data)
+
+  res.send(entity)
+})
+
+// create lobby
+app.post('/lobby', async (req, res) => {
+  const { game } = req.body
+
+  const entity = await Lobby.create({ game })
+  const lobbys = await Lobby.findAll({ include: [User] })
+
+  const data = JSON.stringify(lobbys)
+
+  stream.updateInit(data)
+  stream.send(data)
+
+  res.send(entity)
+})
+
+// user in lobby
+app.put('/user/:userId/lobby/', async (req, res) => {
+  const { userId } = req.params
+  const { id } = req.body
+
+  const userInLobby = await User.findByPk(userId).then(user =>
+    user.update({ lobbyId: id })
+  )
+
+  const data = JSON.stringify(userInLobby)
+
+  stream.updateInit(data)
+  stream.send(data)
+
+  res.send(userInLobby)
+})
+
+app.listen(port, () => console.log(`Listening ${port}`))
