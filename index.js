391494b7a59d584c665daa89fedeef6e28d41774
nicolaurech <urechnicola@gmail.com>
2019-08-13 12:23:42 +0200

    Add authentication to lobby endpoint

--- index.js
+++ index.js
@@ -112,19 +112,44 @@ app.post('/login', async (req, res) => {
   return res.send({ data: 'BAD REQUEST' })
 })
 
+// Authenticate header
+const authenticate = header => {
+  const verifyJWT = token => {
+    return verify(token, process.env.SECRET_KEY, function(err) {
+      if (err) {
+        return false
+      } else {
+        return true
+      }
+    })
+  }
+
+  if (header && header.startsWith('Bearer')) {
+    const [ , token ] = header.split(' ')
+    const success = verifyJWT(token)
+    return success
+  }
+}
+
 // create lobby
 app.post('/lobby', async (req, res) => {
   const { game } = req.body
-
-  const entity = await Lobby.create({ game })
-  const lobbys = await Lobby.findAll({ include: [User] })
-
-  const data = JSON.stringify(lobbys)
-
-  stream.updateInit(data)
-  stream.send(data)
-
-  res.send(entity)
+  const header = req.headers['authorization ']
+  const success = authenticate(header)
+    console.log(success)
+    if (success) {
+      const entity = await Lobby.create({ game })
+      const lobbys = await Lobby.findAll({ include: [User] })
+      const data = JSON.stringify(lobbys)
+      stream.updateInit(data)
+      stream.send(data)
+      return res.send(entity)
+    }
+    const lobbys = await Lobby.findAll({ include: [User] })
+    const data = JSON.stringify(lobbys)
+    stream.updateInit(data)
+    stream.send(data)
+    return res.send({ message: 'Not authorized' })
 })
 
 // user in lobby
