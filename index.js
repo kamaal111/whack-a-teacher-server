b768ef632196fa11f5b8039ec418a3e469b60192
nicolaurech <urechnicola@gmail.com>
2019-08-14 10:44:06 +0200

    Add timestamps

--- index.js
+++ index.js
@@ -35,7 +35,9 @@ const Lobby = db.define('lobby', {
   game: Sequelize.STRING,
   playerOneScore: Sequelize.INTEGER,
   playerTwoScore: Sequelize.INTEGER
-})
+},
+  { timestamps: false }
+)
 
 User.belongsTo(Lobby)
 Lobby.hasMany(User)
